ced68717c3c55894cbc8a707ca2572d6a3d57d35
Andras Petres <petres.andras@gmail.com>
2019-03-15 11:40:15 +0200

    Use gradleUserHomeDir for cache dir
    Since Gradle home [can be changed](http://mrhaki.blogspot.com/2010/09/gradle-goodness-changing-gradle-user.html).

--- src/main/groovy/com/ullink/gradle/sonar/SonarScannerMsbuildPlugin.groovy
+++ src/main/groovy/com/ullink/gradle/sonar/SonarScannerMsbuildPlugin.groovy
@@ -16,12 +16,12 @@ class SonarScannerMsbuildPlugin implements Plugin<Project> {
 
     static final DEFAULT_SCANNER_VERSION = '4.6.0.1930'
     static final DEFAULT_MSBUILD_TASK_NAME = 'msbuild'
-    static final File CACHE_DIR = new File(System.getProperty('user.home'), '.gradle/caches/sonar-scanner-msbuild')
     static final PROJECT_KEY_ARG = 'sonar.projectKey'
     static final MANDATORY_ARGS = [n: 'sonar.projectName', v: 'sonar.projectVersion']
 
     @Override
     void apply(Project project) {
+        def cacheDir = new File(project.gradle.gradleUserHomeDir, 'caches/sonar-scanner-msbuild')
         def scannerExtension = project.extensions.create('sonarqube', SonarScannerMsbuildExtension)
         project.afterEvaluate {
             def msbuild = project.tasks[scannerExtension.msbuildTaskName ?: DEFAULT_MSBUILD_TASK_NAME]
@@ -31,7 +31,7 @@ class SonarScannerMsbuildPlugin implements Plugin<Project> {
             } else {
                 def scannerVersion = scannerExtension.scannerVersion ?: DEFAULT_SCANNER_VERSION
                 def scannerZipName = "sonar-scanner-msbuild-${scannerVersion}-net46.zip"
-                def scannerDir = new File(CACHE_DIR, scannerVersion)
+                def scannerDir = new File(cacheDir, scannerVersion)
                 def sonarScanner = new File(scannerDir, 'SonarScanner.MSBuild.exe')
 
                 // Since some properties may not be evaluated at this moment, let's initialize the arguments
@@ -52,12 +52,12 @@ class SonarScannerMsbuildPlugin implements Plugin<Project> {
                 project.task('sonar', dependsOn: sonarScannerTask) // alias for sonarqube
 
                 if (!sonarScanner.exists()) {
-                    CACHE_DIR.mkdirs()
+                    cacheDir.mkdirs()
                     def scannerDownloadTask = project.task('downloadSonarScannerMsbuild', type: Download) {
                         src "https://github.com/SonarSource/sonar-scanner-msbuild/releases/download/${scannerVersion}/${scannerZipName}"
                         dest File.createTempDir()
                     }
-                    def scannerCacheDir = new File(CACHE_DIR, scannerVersion)
+                    def scannerCacheDir = new File(cacheDir, scannerVersion)
                     scannerCacheDir.mkdir()
                     def unzipTask = project.task('unzipSonarScannerMsbuild', type: Copy, dependsOn: scannerDownloadTask) {
                         from project.zipTree(new File(scannerDownloadTask.dest, scannerZipName))
