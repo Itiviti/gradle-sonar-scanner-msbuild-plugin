576e7cdab63519369517bc638b32f37e27a05c53
Andras Petres <petres.andras@gmail.com>
2019-03-20 11:07:08 +0200

    Init sonar scanner at evaluation time
    Rework the way sonar-scanner-msbuild is initialized in order to:
     - execute init only if sonarqube is needed
     - get rid of the afterEvaluate block
     - get rid of the msbuild task dependency

--- src/main/groovy/com/ullink/gradle/sonar/SonarScannerMsbuildExtension.groovy
+++ src/main/groovy/com/ullink/gradle/sonar/SonarScannerMsbuildExtension.groovy
@@ -1,17 +1,7 @@
 package com.ullink.gradle.sonar
 
 class SonarScannerMsbuildExtension {
-    String scannerVersion
-    String msbuildTaskName
     SonarProperties properties = new SonarProperties()
-
-    void scannerVerion(String scannerVersion) {
-        this.scannerVersion = scannerVersion
-    }
-
-    void msbuildTaskName(String name) {
-        this.msbuildTaskName = name
-    }
 
     // imitate the syntax of org.sonarqube plugin
     void properties(@DelegatesTo(value = SonarProperties, strategy = Closure.DELEGATE_FIRST) Closure sonarProperties) {
--- src/main/groovy/com/ullink/gradle/sonar/SonarScannerMsbuildPlugin.groovy
+++ src/main/groovy/com/ullink/gradle/sonar/SonarScannerMsbuildPlugin.groovy
@@ -1,78 +1,84 @@
 package com.ullink.gradle.sonar
 
-import de.undercouch.gradle.tasks.download.Download
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.tasks.Copy
 import org.gradle.api.tasks.Exec
 
 /**
  * Uses SonarScanner for MSBuild to run the Sonarqube analysis.
  * See https://docs.sonarqube.org/display/SCAN/Analyzing+with+SonarQube+Scanner+for+MSBuild
  * Sonar properties can be configured using the same syntax as for the org.sonarqube plugin
- * in order ro keep compatibility.
+ * in order to keep compatibility.
  */
 class SonarScannerMsbuildPlugin implements Plugin<Project> {
 
-    static final DEFAULT_SCANNER_VERSION = '4.6.0.1930'
-    static final DEFAULT_MSBUILD_TASK_NAME = 'msbuild'
-    static final INIT_SONAR_SCANNER_TASK_NAME = 'initSonarScanner'
+    static final SONAR_SCANNER_GITHUB_URL = 'https://github.com/SonarSource/sonar-scanner-msbuild'
+    static final SONAR_SCANNER_VERSION = '4.6.0.1930'
+    static final SONAR_SCANNER_ZIP = "sonar-scanner-msbuild-${SONAR_SCANNER_VERSION}-net46.zip"
+    static final SONAR_SCANNER_EXE = 'SonarScanner.MSBuild.exe'
+    // keep the 'sonarqube' name for compatibility with the org.sonarqube plugin
+    static final SONAR_SCANNER_TASK_NAME = 'sonarqube'
     static final PROJECT_KEY_ARG = 'sonar.projectKey'
     static final MANDATORY_ARGS = [n: 'sonar.projectName', v: 'sonar.projectVersion']
 
     @Override
     void apply(Project project) {
-        def cacheDir = new File(project.gradle.gradleUserHomeDir, 'caches/sonar-scanner-msbuild')
         def scannerExtension = project.extensions.create('sonarqube', SonarScannerMsbuildExtension)
-        project.afterEvaluate {
-            def msbuild = project.tasks[scannerExtension.msbuildTaskName ?: DEFAULT_MSBUILD_TASK_NAME]
-            if (msbuild == null) {
-                project.logger.warn('Not setting up sonar-msbuild-scanner since no msbuild task was found. ' +
-                    "Please specify the name of the msbuild task using the 'msbuildTaskName' method.")
-            } else {
-                def scannerVersion = scannerExtension.scannerVersion ?: DEFAULT_SCANNER_VERSION
-                def scannerZipName = "sonar-scanner-msbuild-${scannerVersion}-net46.zip"
-                def scannerDir = new File(cacheDir, scannerVersion)
-                def sonarScanner = new File(scannerDir, 'SonarScanner.MSBuild.exe')
 
-                // Since some properties may not be evaluated at this moment, let's initialize the arguments
-                // right before the init task's execution
-                def configureArgsTask = project.tasks.register('configureSonarProperties') {
-                    doLast {
-                        project.tasks.named(INIT_SONAR_SCANNER_TASK_NAME).get().configure {
-                            commandLine += buildArgs(project, scannerExtension.properties.properties)
-                        }
-                    }
-                }
-                def initSonarScannerTask = project.tasks.register(INIT_SONAR_SCANNER_TASK_NAME, Exec) {
-                    dependsOn configureArgsTask
-                    commandLine = [sonarScanner, 'begin']
-                }
+        project.tasks.register(SONAR_SCANNER_TASK_NAME, Exec) {
+            commandLine = [getSonarScannerFile(project), 'end']
+        }
 
-                // keep the 'sonarqube' name for compatibility
-                def sonarScannerTask = project.tasks.register('sonarqube', Exec) {
-                    commandLine = [sonarScanner, 'end']
-                }
-
-                if (!sonarScanner.exists()) {
-                    cacheDir.mkdirs()
-                    def scannerDownloadTask = project.tasks.register('downloadSonarScannerMsbuild', Download) {
-                        src "https://github.com/SonarSource/sonar-scanner-msbuild/releases/download/${scannerVersion}/${scannerZipName}"
-                        dest File.createTempDir()
-                    }
-                    def scannerCacheDir = new File(cacheDir, scannerVersion)
-                    scannerCacheDir.mkdir()
-                    def unzipTask = project.tasks.register('unzipSonarScannerMsbuild', Copy) {
-                        dependsOn scannerDownloadTask
-                        from project.zipTree(new File(scannerDownloadTask.dest, scannerZipName))
-                        into scannerCacheDir
-                    }
-                    initSonarScannerTask.dependsOn unzipTask
-                }
-                msbuild.dependsOn initSonarScannerTask
-                msbuild.finalizedBy sonarScannerTask
+        // We must make sure that sonar-scanner-msbuild is initialized before started.
+        // It cannot be done by tasks because we don't know if sonarqube task is declared or not
+        // before the task graph is ready. That is why the initialization is done at evaluation time.
+        project.gradle.taskGraph.whenReady { graph ->
+            if (graph.hasTask("${project.path}:${SONAR_SCANNER_TASK_NAME}")) {
+                project.logger.info("${SONAR_SCANNER_TASK_NAME} task was detected. Initializing sonar-scanner-msbuild...")
+                initSonarScanner(project, scannerExtension)
             }
         }
+    }
+
+    private void initSonarScanner(Project project, SonarScannerMsbuildExtension extension) {
+        downloadSonarScanner(project)
+        project.exec {
+            commandLine = [getSonarScannerFile(project), 'begin'] + buildArgs(project, extension.properties.properties)
+        }
+    }
+
+    private void downloadSonarScanner(Project project) {
+        def sonarScanner = getSonarScannerFile(project)
+        if (sonarScanner.exists()) {
+            project.logger.info("Not downloading sonar-scanner-msbuild ${SONAR_SCANNER_VERSION} because it " +
+                "already exists in cache ${getCacheDir(project).path}.")
+        } else {
+            def tempDir = File.createTempDir()
+            project.download {
+                src "${SONAR_SCANNER_GITHUB_URL}/releases/download/${SONAR_SCANNER_VERSION}/${SONAR_SCANNER_ZIP}"
+                dest tempDir
+            }
+            def scannerDir = getVersionCacheDir(project)
+            scannerDir.mkdir()
+            project.copy {
+                from project.zipTree(new File(tempDir, SONAR_SCANNER_ZIP))
+                into scannerDir
+            }
+            project.logger.info("sonar-scanner-msbuild ${SONAR_SCANNER_VERSION} has been downloaded into " +
+                getCacheDir(project).path)
+        }
+    }
+
+    private static def getCacheDir(Project project) {
+        new File(project.gradle.gradleUserHomeDir, 'caches/sonar-scanner-msbuild')
+    }
+
+    private static def getVersionCacheDir(Project project) {
+        new File(getCacheDir(project), SONAR_SCANNER_VERSION)
+    }
+
+    private static def getSonarScannerFile(Project project) {
+        new File(getVersionCacheDir(project), SONAR_SCANNER_EXE)
     }
 
     private static def buildArgs(Project project, Map<String, Object> properties) {
